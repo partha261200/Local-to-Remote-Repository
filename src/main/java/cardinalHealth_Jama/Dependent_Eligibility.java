025acd4c4818f7b50a4b4255754d95eec22b11cb
Parthasarathy2612 <parthasarathy5260@gmail.com>
2024-10-15 17:46:19 +0530

    Moving prj from Local to Remote Rep

---
+++ src/main/java/cardinalHealth_Jama/Dependent_Eligibility.java
@@ -0,0 +1,10 @@
+package cardinalHealth_Jama;
+
+public class Dependent_Eligibility {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/admin_Event/HMACVM_Test.java
@@ -0,0 +1,10 @@
+package admin_Event;
+
+public class HMACVM_Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
